9d3098259a40d4aa9e40de0c7f315cbb7b01e5ba
suchil19 <jlsuchil@gmail.com>
2025-05-23 18:32:09 -0600

    08

--- js/buscador.js
+++ js/buscador.js
@@ -1,12 +1,12 @@
 document.getElementById('search').addEventListener('input', function() {
     let filter = this.value.toLowerCase();
-    let cards = document.querySelectorAll('.card');
+    let tarjetas__productos = document.querySelectorAll('.tarjetas__productos');
 
-    cards.forEach(card => {
-        if (card.dataset.id.includes(filter) || card.innerText.toLowerCase().includes(filter)) {
-            card.style.display = "block";
+    tarjetas__productos.forEach(tarjetas__productos => {
+        if (tarjetas__productos.dataset.id.includes(filter) || tarjetas__productos.innerText.toLowerCase().includes(filter)) {
+            tarjetas__productos.style.display = "block";
         } else {
-            card.style.display = "none";
+            tarjetas__productos.style.display = "none";
         }
     });
 });
